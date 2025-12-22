0c06f0991602c4787cd4d0c22b79d1bde3d9101a
harshilbhatia <harshilbhatia2001@gmail.com>
2025-12-22 01:22:05 -0500

    del

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
-git branch -m new main
-git fetch origin
-git branch -u origin/main main
-git remote set-head origin -a
+# git branch -m new main
+# git fetch origin
+# git branch -u origin/main main
+# git remote set-head origin -a
+
+git add . 
+git commit -m "update blog"
+git push origin main.
