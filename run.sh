f0568bfc86a69d62bbd84a56e3344a5a8db2dfb2
harshilbhatia <harshilbhatia2001@gmail.com>
2025-12-22 00:37:16 -0500

    l

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ git init
 git add .
 git commit -m "Initial commit"
 git branch -M main
-git remote add origin https://github.com/HarshilBhatia/HarshilBhatia.github.io.git
+git remote set-url origin git@github.com:HarshilBhatia/HarshilBhatia.github.io.git
 git push -u origin main
